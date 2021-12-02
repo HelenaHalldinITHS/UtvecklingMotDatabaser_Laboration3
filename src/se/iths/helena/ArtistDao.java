83f04a40275cfd7f09c608031ba61827ecc7fff8
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-02 15:27:49 +0100

    adds space between methods in ArtistDao

--- src/se/iths/helena/ArtistDao.java
+++ src/se/iths/helena/ArtistDao.java
@@ -5,11 +5,18 @@ import java.util.Optional;
 
 public interface ArtistDao {
     void add(Artist artist);
+
     void delete(Artist artist);
+
     void update(Artist artist, int age);
+
     void update(Artist artist, String lastName);
+
     void showAll();
+
     Optional<Artist> findById(int id);
+
     List<Artist> findByAge(int age);
+
     List<Artist> findByName(String lastName);
 }
