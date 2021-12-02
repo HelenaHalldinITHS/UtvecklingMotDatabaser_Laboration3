79a23a50acabbbfdef4434e79ebe6084ab5056b6
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-02 11:51:10 +0100

    Adds code for starting application in non-static mode

--- src/se/iths/helena/ArtistApplication.java
+++ src/se/iths/helena/ArtistApplication.java
@@ -1,7 +1,13 @@
 package se.iths.helena;
 
+
 public class ArtistApplication {
     public static void main(String[] args) {
-
+        ArtistApplication application = new ArtistApplication();
+        application.start();
     }
+
+    private void start() {
+    }
+
 }
