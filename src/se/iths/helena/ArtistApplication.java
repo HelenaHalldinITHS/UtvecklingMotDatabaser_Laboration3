cf1c331f6d35d3e7012ea0e5bfb7bc74a3cce2d7
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-02 14:23:54 +0100

    Adds guard against wrong format of input (when looking for a integer)

--- src/se/iths/helena/ArtistApplication.java
+++ src/se/iths/helena/ArtistApplication.java
@@ -1,13 +1,11 @@
 package se.iths.helena;
 
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 
 public class ArtistApplication {
     Scanner scanner = new Scanner(System.in);
-
 
     public static void main(String[] args) {
         ArtistApplication application = new ArtistApplication();
@@ -16,7 +14,6 @@ public class ArtistApplication {
 
     private void start() {
         ArtistDao artistDao = new ArtistDaoImpl();
-
         while (true) {
             printMenu();
             int choice = getChoice();
@@ -44,7 +41,7 @@ public class ArtistApplication {
     private void findArtistByName(ArtistDao artistDao) {
         System.out.println("enter the last name of the artist/artists you want to find: ");
         String lastName = scanner.nextLine();
-        List <Artist> artists = artistDao.findByName(lastName);
+        List<Artist> artists = artistDao.findByName(lastName);
         if (artists.isEmpty())
             System.out.println("there is no current artist with the last name: " + lastName);
         else {
@@ -55,8 +52,8 @@ public class ArtistApplication {
 
     private void findArtistByAge(ArtistDao artistDao) {
         System.out.println("enter the age of the artist/artists you want to find: ");
-        int age = Integer.parseInt(scanner.nextLine());
-        List <Artist> artists = artistDao.findByAge(age);
+        int age = getIntInput();
+        List<Artist> artists = artistDao.findByAge(age);
         if (artists.isEmpty())
             System.out.println("there is no current artist with age: " + age);
         else {
@@ -67,17 +64,18 @@ public class ArtistApplication {
 
     private void findArtistById(ArtistDao artistDao) {
         System.out.println("enter id of the artist you want to find: ");
-        int id = Integer.parseInt(scanner.nextLine());
-        artistDao.findById(id).ifPresentOrElse(this::print,() -> System.out.println("there is no current artist with id " + id));
+        int id = getIntInput();
+        artistDao.findById(id).ifPresentOrElse(this::print, () -> System.out.println("there is no current artist with id: " + id));
         System.out.println();
     }
 
-    private void print(Artist artist){
+    private void print(Artist artist) {
         System.out.println("id: " + artist.getId() +
                 ", first name: " + artist.getFirstName() +
                 ", last name: " + artist.getLastName() +
                 ", age: " + artist.getAge());
     }
+
     private void showAllArtists(ArtistDao artistDao) {
         artistDao.showAll();
         System.out.println();
@@ -85,38 +83,38 @@ public class ArtistApplication {
 
     private void updateArtistName(ArtistDao artistDao) {
         System.out.println("enter id of the artist you want to update: ");
-        int id = Integer.parseInt(scanner.nextLine());
+        int id = getIntInput();
         System.out.println("enter the artist's new last name: ");
         String lastName = scanner.nextLine();
-        artistDao.findById(id).ifPresentOrElse(artist -> artistDao.update(artist, lastName), () -> System.out.println("there is no current artist with id " + id));
+        artistDao.findById(id).ifPresentOrElse(artist -> artistDao.update(artist, lastName), () -> System.out.println("there is no current artist with id: " + id));
         System.out.println();
     }
 
     private void updateArtistAge(ArtistDao artistDao) {
         System.out.println("enter id of the artist you want to update: ");
-        int id = Integer.parseInt(scanner.nextLine());
+        int id = getIntInput();
         System.out.println("enter the artist's new age: ");
-        int age = Integer.parseInt(scanner.nextLine());
-        artistDao.findById(id).ifPresentOrElse(artist -> artistDao.update(artist, age), () -> System.out.println("there is no current artist with id " + id));
+        int age = getIntInput();
+        artistDao.findById(id).ifPresentOrElse(artist -> artistDao.update(artist, age), () -> System.out.println("there is no current artist with id: " + id));
         System.out.println();
     }
 
     private void removeArtist(ArtistDao artistDao) {
         System.out.println("enter id of the artist you want to remove: ");
-        int id = Integer.parseInt(scanner.nextLine());
+        int id = getIntInput();
         artistDao.findById(id).ifPresentOrElse(artistDao::delete, () -> System.out.println("there is no current artist with id " + id));
         System.out.println();
     }
 
     private void addArtist(ArtistDao artistDao) {
         System.out.println("enter the artist's id:");
-        int id = Integer.parseInt(scanner.nextLine());
+        int id = getIntInput();
         System.out.println("enter the artist's first name: ");
         String firstName = scanner.nextLine();
         System.out.println("enter the artist's last name: ");
         String lastName = scanner.nextLine();
         System.out.println("enter the artist's age:");
-        int age = Integer.parseInt(scanner.nextLine());
+        int age = getIntInput();
         artistDao.add(new Artist(id, firstName, lastName, age));
         System.out.println();
     }
@@ -137,6 +135,21 @@ public class ArtistApplication {
     }
 
     private int getChoice() {
-        return Integer.parseInt(scanner.nextLine());
+        return getIntInput();
     }
+
+
+    private int getIntInput() {
+        int input;
+        while (true) {
+            try {
+                input = Integer.parseInt(scanner.nextLine());
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("input must be a number, try again:");
+            }
+        }
+        return input;
+    }
+
 }
