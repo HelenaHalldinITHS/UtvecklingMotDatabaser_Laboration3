0b0388fc9182e90d6c232e9c8406a6b632b246d8
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-02 11:50:12 +0100

    Adds interface ArtistDao

---
+++ src/se/iths/helena/ArtistDao.java
@@ -0,0 +1,15 @@
+package se.iths.helena;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ArtistDao {
+    void add(Artist artist);
+    void delete(Artist artist);
+    void update(Artist artist, int age);
+    void update(Artist artist, String lastName);
+    void showAll();
+    Optional<Artist> findById(int id);
+    List<Artist> findByAge(int age);
+    List<Artist> findByName(String lastName);
+}
