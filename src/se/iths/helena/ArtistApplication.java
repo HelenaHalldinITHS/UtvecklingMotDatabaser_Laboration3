c6ddd500a49f4c7735f346ccd64e4fa3c1acf603
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-02 13:33:42 +0100

    Adds methods for finding an artist by its id through consol application

--- src/se/iths/helena/ArtistApplication.java
+++ src/se/iths/helena/ArtistApplication.java
@@ -16,22 +16,42 @@ public class ArtistApplication {
     private void start() {
         ArtistDao artistDao = new ArtistDaoImpl();
 
-        while (true){
-         printMenu();
-         int choice = getChoice();
-         if (choice == 0)
-             break;
-         executeChoice(choice,artistDao);
+        while (true) {
+            printMenu();
+            int choice = getChoice();
+            if (choice == 0)
+                break;
+            executeChoice(choice, artistDao);
         }
     }
 
-    private void executeChoice(int i, ArtistDao artistDao){
-        switch (i){
+    private void executeChoice(int i, ArtistDao artistDao) {
+        switch (i) {
             case 1 -> addArtist(artistDao);
             case 2 -> removeArtist(artistDao);
             case 3 -> updateArtistAge(artistDao);
             case 4 -> updateArtistName(artistDao);
+            case 5 -> showAllArtists(artistDao);
+            case 6 -> findArtistById(artistDao);
         }
+    }
+
+    private void findArtistById(ArtistDao artistDao) {
+        System.out.println("enter id of the artist you want to find: ");
+        int id = Integer.parseInt(scanner.nextLine());
+        artistDao.findById(id).ifPresentOrElse(this::print,() -> System.out.println("there is no current artist with id " + id));
+        System.out.println();
+    }
+
+    private void print(Artist artist){
+        System.out.println("id: " + artist.getId() +
+                ", first name: " + artist.getFirstName() +
+                ", last name: " + artist.getLastName() +
+                ", age: " + artist.getAge());
+    }
+    private void showAllArtists(ArtistDao artistDao) {
+        artistDao.showAll();
+        System.out.println();
     }
 
     private void updateArtistName(ArtistDao artistDao) {
@@ -39,7 +59,8 @@ public class ArtistApplication {
         int id = Integer.parseInt(scanner.nextLine());
         System.out.println("enter the artist's new last name: ");
         String lastName = scanner.nextLine();
-        artistDao.findById(id).ifPresentOrElse(artist -> artistDao.update(artist,lastName), () -> System.out.println("there is no current artist with id " + id));
+        artistDao.findById(id).ifPresentOrElse(artist -> artistDao.update(artist, lastName), () -> System.out.println("there is no current artist with id " + id));
+        System.out.println();
     }
 
     private void updateArtistAge(ArtistDao artistDao) {
@@ -47,13 +68,15 @@ public class ArtistApplication {
         int id = Integer.parseInt(scanner.nextLine());
         System.out.println("enter the artist's new age: ");
         int age = Integer.parseInt(scanner.nextLine());
-        artistDao.findById(id).ifPresentOrElse(artist -> artistDao.update(artist,age), () -> System.out.println("there is no current artist with id " + id));
+        artistDao.findById(id).ifPresentOrElse(artist -> artistDao.update(artist, age), () -> System.out.println("there is no current artist with id " + id));
+        System.out.println();
     }
 
     private void removeArtist(ArtistDao artistDao) {
         System.out.println("enter id of the artist you want to remove: ");
         int id = Integer.parseInt(scanner.nextLine());
         artistDao.findById(id).ifPresentOrElse(artistDao::delete, () -> System.out.println("there is no current artist with id " + id));
+        System.out.println();
     }
 
     private void addArtist(ArtistDao artistDao) {
@@ -65,24 +88,26 @@ public class ArtistApplication {
         String lastName = scanner.nextLine();
         System.out.println("enter the artist's age:");
         int age = Integer.parseInt(scanner.nextLine());
-        artistDao.add(new Artist(id,firstName,lastName,age));
+        artistDao.add(new Artist(id, firstName, lastName, age));
+        System.out.println();
     }
 
     private void printMenu() {
-        System.out.println("""
+        System.out.println(""" 
                 Choose one of the following (by writing its corresponding number):
                 1. Add an artist
                 2. Remove an artist
                 3. Update an artist's age
                 4. Update an artist's lastname
-                4. Show all artists
-                5. Find artist by id
-                6. Find artist by age
-                7. Find artist by name
+                5. Show all artists
+                6. Find artist by id
+                7. Find artist by age
+                8. Find artist by name
                 0. End application
                 """);
     }
-    private int getChoice(){
+
+    private int getChoice() {
         return Integer.parseInt(scanner.nextLine());
     }
 }
